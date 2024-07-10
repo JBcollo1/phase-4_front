cbeb603e368ad8a9fb759a88ec04dcf2c8d0463a
Collins <jbcollins254@gmail.com>
2024-07-10 16:28:47 +0300

    add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+
+
+const Home = () =>{
+    return (
+        <div className="home-container">
+            <h2>NovelNest</h2>
+            <h1>Welcome to NovelNest</h1>
+            <p>Discover new worlds, dive into fascinating stories, and share your reading adventures. Connect with fellow book lovers and enrich your reading journey right here at NovelNest.</p>
+
+            <Link to="/collection"><button>Explore Collection</button></Link>
+
+
+        </div>
+    )
+}
+export default Home
